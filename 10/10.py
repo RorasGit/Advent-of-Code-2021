c8cfc7f9eb45e8c86b243e48894d7e508f7d73c8
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-10 10:55:19 +0100

    cleaned up

--- 10/10.py
+++ 10/10.py
@@ -1,47 +1,37 @@
 from collections import Counter
 
-val = {")": 3, "]":57, "}": 1197, ">":25137}
-val2 = {")": 1, "]":2, "}": 3, ">":4}
+values = {")": 3, "]":57, "}": 1197, ">":25137, "(": 1, "[":2, "{": 3, "<":4}
 
-def value(S):
+def value(line):
     sum=0
-    C = Counter(S)
-    for c in val.keys():
-        sum+= C[c]*val[c]
-    return sum
-
-def value2(S):
-    sum=0
-    for c in S:
-        sum = sum * 5 + val2[c]
+    for c in line:
+        sum = sum * 5 + values[c]
     return sum
 
 with open("input.txt") as f:
     
     chunks = f.read().splitlines()
-    types = {")":"(", "}":"{", ">":"<", "]": "["}
-    typesInv = {v: k for k, v in types.items()}
-    fail = ""
+    types = {"(":")", "{":"}", "<":">", "[": "]"}
+    failedRows = Counter()
+    failedSum = 0
     unfinished = []
     
     for line in chunks:
         closeable = []
         ok = True
         for c in line:
-            if c in types.values():
+            if c in types:
                 closeable.append(c)
             else:
-                if types[c] == closeable[-1]:
-                    closeable.pop()
-                else:
+                if types[closeable.pop()] != c:
                     ok = False
-                    fail += c
+                    failedSum += values[c]
                     break
         if(ok):
             unfinished.append(closeable)        
 
-    sums = [value2([typesInv[x] for x in line[::-1]]) for line in unfinished]
+    sums = [value([x for x in line[::-1]]) for line in unfinished]
     sums.sort()
 
-    print(value(fail))
+    print(failedSum)
     print(sums[len(sums)//2])
