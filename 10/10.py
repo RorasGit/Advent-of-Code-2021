d53a23048a95ad5d5c0888b6dd60650b6e4e0277
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-10 13:24:58 +0100

    reduce function

--- 10/10.py
+++ 10/10.py
@@ -1,33 +1,23 @@
-values = {")": 3, "]":57, "}": 1197, ">": 25137, "(": 1, "[":2, "{": 3, "<": 4}
 
-def value(line):
-    sum=0
+from functools import reduce
+from statistics import median
+
+values = {")": 3, "]":57, "}": 1197, ">": 25137, "(": 1, "[":2, "{": 3, "<": 4}
+types = {"(":")", "{":"}", "<":">", "[": "]"}
+
+def score(line):
+    closeable = []
     for c in line:
-        sum = sum * 5 + values[c]
-    return sum
+        if c in types:
+            closeable.append(c)
+        else:
+            if types[closeable.pop()] != c:
+                return values[c], None
+    return 0, reduce(lambda x, y: x * 5 + values[y], reversed(closeable), 0)
 
 with open("input.txt") as f:
     
-    chunks = f.read().splitlines()
-    types = {"(":")", "{":"}", "<":">", "[": "]"}
-    failedSum = 0
-    unfinished = []
-    
-    for line in chunks:
-        closeable = []
-        ok = True
-        for c in line:
-            if c in types:
-                closeable.append(c)
-            else:
-                if types[closeable.pop()] != c:
-                    ok = False
-                    failedSum += values[c]
-                    break
-        if(ok):
-            unfinished.append(closeable)        
+    scores = [score(line) for line in f.read().splitlines()]       
 
-    incompleteSum = sorted([value(reversed(line)) for line in unfinished])
-
-    print(failedSum)
-    print(incompleteSum[len(incompleteSum)//2])
+    print(sum([corrupted for corrupted, _ in scores]))
+    print(median([incomplete for _, incomplete in scores if incomplete]))
