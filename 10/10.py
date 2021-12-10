69266589b7ad20b8db2b43d8e265a622af83b6de
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-10 14:18:02 +0100

    renamed variables

--- 10/10.py
+++ 10/10.py
@@ -2,18 +2,18 @@
 from functools import reduce
 from statistics import median
 
-values = {")": 3, "]":57, "}": 1197, ">": 25137, "(": 1, "[":2, "{": 3, "<": 4}
-types = {"(":")", "{":"}", "<":">", "[": "]"}
+value = {"(": 1, "[": 2, "{": 3, "<": 4, ")": 3, "]": 57, "}": 1197, ">": 25137}
+chunk = {"(": ")", "{": "}", "<": ">", "[": "]"}
 
 def score(line):
-    closeable = []
+    openedChunks = []
     for c in line:
-        if c in types:
-            closeable.append(c)
+        if c in chunk:
+            openedChunks.append(c)
         else:
-            if types[closeable.pop()] != c:
-                return True, values[c]
-    return False, reduce(lambda x, y: x * 5 + values[y], reversed(closeable), 0)
+            if chunk[openedChunks.pop()] != c:
+                return True, value[c]
+    return False, reduce(lambda x, y: x * 5 + value[y], reversed(openedChunks), 0)
 
 with open("input.txt") as f:
     
