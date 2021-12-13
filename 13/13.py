518feaca6be33a80dda5644a82c9f9d3b126c96a
Daniel Augurell <daniel.augurell@gmail.com>
2021-12-13 10:52:07 +0100

    cleaned up

--- 13/13.py
+++ 13/13.py
@@ -1,50 +1,37 @@
-from operator import itemgetter
-def flipGrid(grid, value, vertical, maxX, maxY):
-    for y in range(maxY):
-        for x in range (maxX):
-            if(grid[x][y] > 0):
-                newX = x
-                newY = y
-                if(vertical):
-                    newX = x - (x - value)*2
-                else:
-                    newY = y - (y - value)*2
-                grid[x][y] = 0
-                grid[newX][newY] = 1
-
+def flipGrid(grid, value, vertical):
+    newGrid = {}
+    for (x, y) in grid:
+        newX = x
+        newY = y
+        if(vertical and value < x):
+            newX = value - (x - value)
+        elif(not vertical and value < y):
+            newY = value - (y - value)
+        newGrid[(newX, newY)] = True
+    return newGrid
 with open("input.txt") as f:
     
     input, instructions = [line for line in f.read().split("\n\n")]
-    
     instructions = [line[11:].split("=") for line in instructions.split("\n")]
-    input = [tuple(int(x) for x in line.split(",")) for line in input.split("\n")]
-    maxX = max(input,key=itemgetter(0))[0]+1
-    maxY = max(input,key=itemgetter(1))[1]+1
-    grid = [[0 for _ in range(maxY)] for __ in range(maxX)]
-
-    for x, y in input:
-        grid[x][y] = 1
+    grid =  {tuple(map(int, line.split(","))) : True for line in input.split()}
 
     first = True
     for ins, value in instructions:
-        vertical = ins == "x"
-        flipGrid(grid, int(value), vertical, maxX, maxY)
-        
-        if(vertical):
-            maxX = int(value)
-        else:
-            maxY = int(value)
+        grid = flipGrid(grid, int(value), ins == "x")
         if(first):
-            print(sum(sum(x) for x in grid))
+            print(len(grid))
             first = False
-            
-    for y in range(maxY):
-        for x in range(maxX):
-            if grid[x][y]:
-                print(grid[x][y], end= '')
+
+    lenX = max(x for x,_ in grid)+1
+    lenY = max(y for _,y in grid)+1
+    for y in range(lenY):
+        row = ""
+        for x in range(lenX):
+            if (x, y) in grid:
+                row += "1"
             else:
-                print(" ", end='')
-        print()
+                row += " "
+        print(row)
 
 
 
