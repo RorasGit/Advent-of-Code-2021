516928c935191db27bfdd01d91e83f64d6699143
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 15:25:19 +0100

    pylint fix day3

--- 03/day03.py
+++ 03/day03.py
@@ -1,37 +1,42 @@
-def getOnes(ones : list[str], larger, index = 0):
-    if(len(ones) == 1 ) :
+def get_ones(ones : list[str], larger, index = 0):
+    if len(ones) == 1:
         return ones[0]
 
-    nextOnes = []
-    nextZeros = []
+    next_ones = []
+    next_zeros = []
     for test in ones:
-        if(int(test[index]) == 1):
-            nextOnes.append(test)
-        else:      
-            nextZeros.append(test)
-    testCase = len(nextOnes) >= len(nextZeros)
-    if(testCase if larger else not testCase) :
-        return getOnes(nextOnes, larger, index+1)
-    else :
-        return getOnes(nextZeros, larger, index+1)
-
-with open("input.txt" , 'r') as input :
-    values = input.read().splitlines()
-    sum = dict.fromkeys(range(len(values[0])), 0)
-    for val in values:
-        for c in range(0, len(val)):
-            sum[c] += int(val[c])
-
-    length = len(values)
-    epsilon = 0
-    gamma = 0
-    for size in sum.values():
-        epsilon = epsilon << 1
-        gamma = gamma << 1
-        if size > length/2 :
-            epsilon +=1
+        if int(test[index]) == 1:
+            next_ones.append(test)
         else:
-            gamma+=1
+            next_zeros.append(test)
+    test_case = len(next_ones) >= len(next_zeros)
+    if(test_case if larger else not test_case) :
+        return get_ones(next_ones, larger, index+1)
+    else :
+        return get_ones(next_zeros, larger, index+1)
+def main():
+    with open("input.txt" , 'r', encoding="utf-8") as file :
+        values = file.read().splitlines()
+        amount = dict.fromkeys(range(len(values[0])), 0)
+        for val in values:
+            for i, amount in enumerate(amount):
+                amount[i] += int(val[i])
 
-    print(gamma*epsilon)
-    print(int(getOnes(values, 1), 2)*int(getOnes(values, 0), 2))
+        length = len(values)
+        epsilon = 0
+        gamma = 0
+        for size in amount.values():
+            epsilon = epsilon << 1
+            gamma = gamma << 1
+            if size > length/2 :
+                epsilon +=1
+            else:
+                gamma+=1
+
+        print(gamma*epsilon)
+        print(int(get_ones(values, 1), 2)*int(get_ones(values, 0), 2))
+
+
+if __name__ == '__main__':
+    main()
+    
