cc7ecda28b516018f007033c40a3223b1e2583dd
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 16:28:42 +0100

    pylinted day 8

--- 08/day08.py
+++ 08/day08.py
@@ -1,69 +1,66 @@
 from collections import Counter
 
-def share_n_digits(a, b, n):
-    return sum((Counter(a) & Counter(b)).values()) == n
+def share_n_digits(digit_one, digit_two, number):
+    return sum((Counter(digit_one) & Counter(digit_two)).values()) == number
 
-def share_n_segments(distinct_segment, unknown_segments, n):
+def share_n_segments(distinct_segment, unknown_segments, number):
     for segment in unknown_segments:
-        if share_n_digits(segment, distinct_segment, n):
+        if share_n_digits(segment, distinct_segment, number):
             return segment
+def main():
+    with open("input.txt", "r", encoding="utf-8") as file:
+        lines = file.readlines()
 
-with open("input.txt") as f:
-    lines = f.readlines()
+        segments, output = map(list, zip(*(line.split("|") for line in lines)))
+        output = [line.split() for line in output]
+        segments = [line.split() for line in segments]
+        digit_len = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
+        unqiue_len = [i for i in range(10) if digit_len.count(i) == 1]
 
-    input = []
-    output = []
-    digitLen = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
-    unqiueLen = [i for i in range(10) if digitLen.count(i) == 1]
-    
-    for line in lines:
-        s = line.split("|")
-        input.append(s[0].split())
-        output.append(s[1].split())
+        # Part 1
+        sum_digit = 0
+        for line in output:
+            for digit in line:
+                if len(digit) in unqiue_len:
+                    sum_digit += 1
 
+        print(sum_digit)
 
-    # Part 1
-    sumDigit = 0
-    for line in output:
-        for digit in line:
-            if len(digit) in unqiueLen:
-                sumDigit += 1
-
-    print(sumDigit)
-    
-    #Part 2
-    sumDigit = 0
-    for i in range(len(input)):
-        segment = dict()
-        unknown_segments = dict()
-        for digit in input[i]:  
-            length = len(digit)
-            if length in unqiueLen:
-                segment[digitLen.index(length)] = digit
-            else :
-                if(length in unknown_segments.keys()):
-                    unknown_segments[length].append(digit)
+        #Part 2
+        sum_digit = 0
+        for i, _ in enumerate(segments):
+            segment = {}
+            unknown_segments = {}
+            for digit in segments[i]:
+                length = len(digit)
+                if length in unqiue_len:
+                    segment[digit_len.index(length)] = digit
                 else :
-                    unknown_segments[length] = [digit]
-        segment[3] = share_n_segments(segment[7], unknown_segments[5], 3)
-        unknown_segments[5].remove(segment[3])
-        segment[5] = share_n_segments(segment[4], unknown_segments[5], 3) 
-        unknown_segments[5].remove(segment[5])
-        segment[2] = unknown_segments[5][0]
-        segment[0] = share_n_segments(segment[5], unknown_segments[6], 4)
-        unknown_segments[6].remove(segment[0])
-        segment[9] = share_n_segments(segment[7], unknown_segments[6], 3)
-        unknown_segments[6].remove(segment[9])
-        segment[6] = unknown_segments[6][0]
+                    if length in unknown_segments:
+                        unknown_segments[length].append(digit)
+                    else :
+                        unknown_segments[length] = [digit]
+            segment[3] = share_n_segments(segment[7], unknown_segments[5], 3)
+            unknown_segments[5].remove(segment[3])
+            segment[5] = share_n_segments(segment[4], unknown_segments[5], 3)
+            unknown_segments[5].remove(segment[5])
+            segment[2] = unknown_segments[5][0]
+            segment[0] = share_n_segments(segment[5], unknown_segments[6], 4)
+            unknown_segments[6].remove(segment[0])
+            segment[9] = share_n_segments(segment[7], unknown_segments[6], 3)
+            unknown_segments[6].remove(segment[9])
+            segment[6] = unknown_segments[6][0]
 
-        outputDigit = ""
-        for digit in output[i]:
-            for key, value in segment.items():
-                if Counter(value) == Counter(digit):
-                    outputDigit += str(key)
-        
-        sumDigit += int(outputDigit)
+            output_digit = ""
+            for digit in output[i]:
+                for key, value in segment.items():
+                    if Counter(value) == Counter(digit):
+                        output_digit += str(key)
 
-print(sumDigit)
-        
-        
+            sum_digit += int(output_digit)
+
+    print(sum_digit)
+
+
+if __name__ == '__main__':
+    main()
