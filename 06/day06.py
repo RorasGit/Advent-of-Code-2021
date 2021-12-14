356ce7e37601fce83ede4211e3f83ea30bc6a1ab
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 15:55:46 +0100

    fix day6 pylint

--- 06/day06.py
+++ 06/day06.py
@@ -1,17 +1,20 @@
 from collections import deque
+def main():
+    with open("input.txt", "r", encoding="utf-8") as file:
+        days = 256
+        state = deque([0] * 9)
 
-with open("input.txt") as f:
-    days = 256 
-    state = deque([0] * 9)
+        for fish in next(file).split(","):
+            state[int(fish)] += 1
 
-    for x in next(f).split(","):
-        state[int(x)] += 1
+        for day in range(days):
+            state.rotate(-1)
+            state[6] += state[8]
 
-    for day in range(days):
-        state.rotate(-1)
-        state[6] += state[8]
+            if day == 79:
+                print(sum(state))
 
-        if day == 79:
-            print(sum(state))
+        print(sum(state))
 
-    print(sum(state))
+if __name__ == '__main__':
+    main()
