8d5d21c5b578f7d4bd11d78f854e1a685d18fb24
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 15:19:12 +0100

    day2 fixpylint

--- 02/day02.py
+++ 02/day02.py
@@ -1,37 +1,41 @@
-with open("input.txt" , 'r') as input :
-    values = input.read().splitlines()
-    pos = 0
-    depth = 0
-    for value in values:
-        op, size = value.split(" ")
-        size = int(size)
-        if op == "forward":
-            pos+=size
-        elif op == "down":
-            depth+=size
-        else:
-            if size > depth:
-                depth = 0
+def main():
+    with open("input.txt" , 'r', encoding='utf-8') as file :
+        values = file.read().splitlines()
+        pos = 0
+        depth = 0
+        for value in values:
+            oper, size = value.split(" ")
+            size = int(size)
+            if oper == "forward":
+                pos+=size
+            elif oper == "down":
+                depth+=size
             else:
-                depth -= size
-    
-    print(f"pos: {pos}, depth: {depth}, total: {pos*depth}")
+                if size > depth:
+                    depth = 0
+                else:
+                    depth -= size
 
-    aim = 0
-    pos = 0
-    depth = 0
-    for value in values:
-        op, size = value.split(" ")
-        size = int(size)
-        if op == "up":
-            aim-=size
-        elif op == "down":
-            aim+=size
-        else:
-            pos+=size
-            depth+=aim*size
-            if(depth < 0):
-                depth = 0
-            
+        print(f"pos: {pos}, depth: {depth}, total: {pos*depth}")
+
+        aim = 0
+        pos = 0
+        depth = 0
+        for value in values:
+            oper, size = value.split(" ")
+            size = int(size)
+            if oper == "up":
+                aim-=size
+            elif oper == "down":
+                aim+=size
+            else:
+                pos+=size
+                depth+=aim*size
+                depth = max(depth, 0)
+
+        print(f"pos: {pos}, depth: {depth}, aim: {aim} total: {pos*depth}")
+
+
+if __name__ == '__main__':
+    main()
     
-    print(f"pos: {pos}, depth: {depth}, aim: {aim} total: {pos*depth}")
