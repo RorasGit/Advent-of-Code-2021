ec08518aedd4b4f2c5871378390173dc6bb7a335
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 15:16:22 +0100

    fixed pylint errors in day1

--- 01/day01.py
+++ 01/day01.py
@@ -1,33 +1,33 @@
-with open("input.txt" , 'r') as input :
-    values = input.read().splitlines()
-    inc = -1
-    lastValue = 0
-    for line in values: 
-        if int(line) > lastValue:   
-            inc += 1
-        lastValue = int(line)
-
-    print(inc)
-
-    inc = -3
-    A = 0
-    B = 0
-    C = 0
-    lastValue = 0
-    for line in values: 
-        A = int(line) 
-        sum = A+B+C
-        if sum > lastValue :
-            inc+=1
-        lastValue = sum
-        C = B
-        B = A
 
 
-    print(inc)
+def main():
+    with open("input.txt" , 'r', encoding='utf-8') as file :
+        values = file.read().splitlines()
+        inc = -1
+        last_value = 0
+        for line in values:
+            if int(line) > last_value:
+                inc += 1
+            last_value = int(line)
 
+        print(inc)
 
+        inc = -3
+        one = 0
+        two = 0
+        three = 0
+        last_value = 0
+        for line in values:
+            one = int(line)
+            total = one+two+three
+            if total > last_value :
+                inc+=1
+            last_value = total
+            three = two
+            two = one
 
+        print(inc)
 
-
-
+if __name__ == '__main__':
+    main()
+    
