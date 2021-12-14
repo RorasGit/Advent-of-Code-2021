3f35cc03b8df3a6195214447887d1ca9fd6a648b
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 16:49:12 +0100

    day 13 lintified

--- 13/day13.py
+++ 13/day13.py
@@ -1,36 +1,34 @@
-def flipGrid(grid, value, vertical):
-    newGrid = {}
-    for (x, y) in grid:
-        newX = 2 * value - x if vertical and x > value else x
-        newY = 2 * value - y if not vertical and y > value else y
-        newGrid[(newX, newY)] = True
-    return newGrid
-with open("input.txt") as f:
-    
-    input, instructions = [line for line in f.read().split("\n\n")]
-    instructions = [line[11:].split("=") for line in instructions.split("\n")]
-    grid =  {tuple(map(int, line.split(","))) : True for line in input.split()}
+def flip_grid(grid, value, vertical):
+    new_grid = {}
+    for (x_pos, y_pos) in grid:
+        new_x = 2 * value - x_pos if vertical and x_pos > value else x_pos
+        new_y = 2 * value - y_pos if not vertical and y_pos > value else y_pos
+        new_grid[(new_x, new_y)] = True
+    return new_grid
+def main():
+    with open("input.txt", "r", encoding="utf-8") as file:
 
-    first = True
-    for ins, value in instructions:
-        grid = flipGrid(grid, int(value), ins == "x")
-        if(first):
-            print(len(grid))
-            first = False
+        lines, instructions = list(file.read().split("\n\n"))
+        instructions = [line[11:].split("=") for line in instructions.split("\n")]
+        grid =  {tuple(map(int, line.split(","))) : True for line in lines.split()}
 
-    lenX = max(x for x,_ in grid)
-    lenY = max(y for _,y in grid)
-    for y in range(lenY+1):
-        row = ""
-        for x in range(lenX+1):
-            if (x, y) in grid:
-                row += "1"
-            else:
-                row += " "
-        print(row)
+        first = True
+        for ins, value in instructions:
+            grid = flip_grid(grid, int(value), ins == "x")
+            if first:
+                print(len(grid))
+                first = False
 
+        len_x = max(x for x,_ in grid)
+        len_y = max(y for _,y in grid)
+        for pos_y in range(len_y+1):
+            row = ""
+            for pos_x in range(len_x+1):
+                if (pos_x, pos_y) in grid:
+                    row += "1"
+                else:
+                    row += " "
+            print(row)
 
-
-
-
-        
+if __name__ == '__main__':
+    main()
