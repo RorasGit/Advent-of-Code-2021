526a1ff2cf898d81a59cb6b7c03709b654a5b592
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 10:46:07 +0100

    cleaned up

--- 14/14.py
+++ 14/14.py
@@ -5,10 +5,9 @@ def step(pairs, ins, ans):
 
     newPairs = Counter()
     for pair in pairs:
-        if(pair in ins):
-            newPairs[(pair[0],ins[pair])] += pairs[pair]
-            newPairs[(ins[pair],pair[1])] += pairs[pair]
-            ans[ins[pair]] += pairs[pair]
+        newPairs[(pair[0],ins[pair])] += pairs[pair]
+        newPairs[(ins[pair],pair[1])] += pairs[pair]
+        ans[ins[pair]] += pairs[pair]
     return newPairs
 
 with open("input.txt") as f:
@@ -16,12 +15,10 @@ with open("input.txt") as f:
     template, instructions = [line for line in f.read().split("\n\n")]
     instructions = [line.split(" -> ") for line in instructions.split("\n")]
     instructions = {tuple(s[0]) : s[1] for s in instructions}
-    pairs = Counter()
+
+    pairs = Counter(zip(template, template[1:]))
     ans = Counter(template)
 
-    for a, b in zip(template, template[1:]):
-        pairs[(a,b)] += 1
-    
     for i in range (10):
         pairs = step(pairs, instructions, ans)
 
@@ -31,3 +28,4 @@ with open("input.txt") as f:
         pairs = step(pairs, instructions, ans)
 
     print(max(ans.values()) - min(ans.values()))
+
