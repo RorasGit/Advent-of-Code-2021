9ce6e8d95f742d48a4ddb6e179ca5353fec3eead
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 16:38:18 +0100

    day10 pylinted

--- 10/day10.py
+++ 10/day10.py
@@ -6,17 +6,20 @@ value = {"(": 1, "[": 2, "{": 3, "<": 4, ")": 3, "]": 57, "}": 1197, ">": 25137}
 chunk = {"(": ")", "{": "}", "<": ">", "[": "]"}
 
 def score(line):
-    openedChunks = []
-    for c in line:
-        if c in chunk:
-            openedChunks.append(c)
-        elif chunk[openedChunks.pop()] != c:
-            return True, value[c]
-    return False, reduce(lambda x, y: x * 5 + value[y], reversed(openedChunks), 0)
+    opened_chunks = []
+    for token in line:
+        if token in chunk:
+            opened_chunks.append(token)
+        elif chunk[opened_chunks.pop()] != token:
+            return True, value[token]
+    return False, reduce(lambda x, y: x * 5 + value[y], reversed(opened_chunks), 0)
+def main():
+    with open("input.txt", "r", encoding="utf-8") as file:
 
-with open("input.txt") as f:
-    
-    scores = [score(line) for line in f.read().splitlines()]       
+        scores = [score(line) for line in file.read().splitlines()]
 
-    print(sum(score for corrupted, score in scores if corrupted))
-    print(median(score for corrupted, score in scores if not corrupted))
+        print(sum(score for corrupted, score in scores if corrupted))
+        print(median(score for corrupted, score in scores if not corrupted))
+
+if __name__ == '__main__':
+    main()
