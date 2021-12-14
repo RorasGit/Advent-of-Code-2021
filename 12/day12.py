98bb9450f97a91b48d577fb0d35cd34079c61381
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 16:46:29 +0100

    day12 lintified

--- 12/day12.py
+++ 12/day12.py
@@ -1,25 +1,28 @@
 import networkx as nx
 
-def numberOfPaths(graph, node = "start", path = ["start"], visit_twice = False):
-    if(node == "end"):
+def number_of_paths(graph, node, path, visit_twice = False):
+    if node == "end":
         return 1
     count = 0
     for neighbour in graph.neighbors(node):
         neighbour_visit_twice = visit_twice
-        if(neighbour in path and not neighbour.isupper()):
+        if neighbour in path and not neighbour.isupper():
             if neighbour == "start" or not visit_twice:
                 continue
             else:
                 neighbour_visit_twice = False
-        count += numberOfPaths(graph, neighbour, path + [neighbour], neighbour_visit_twice)
-                    
-    return count                 
+        count += number_of_paths(graph, neighbour, path + [neighbour], neighbour_visit_twice)
 
-with open("input.txt") as f:
+    return count
+def main():
+    with open("input.txt", "r", encoding="utf-8") as file:
+
+        cave_graph = nx.Graph()
+        cave_graph.add_edges_from([line.strip().split("-") for line in file.readlines()])
+
+        print(number_of_paths(cave_graph, "start", ["start"]))
+        print(number_of_paths(cave_graph, "start", ["start"], visit_twice = True))
+
+if __name__ == '__main__':
+    main()
     
-    cave_graph = nx.Graph()
-    cave_graph.add_edges_from([line.strip().split("-") for line in f.readlines()])
-
-    print(numberOfPaths(cave_graph))
-    print(numberOfPaths(cave_graph, visit_twice = True))
-        
