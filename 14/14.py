cef7f8a8fc164b297d223c10731d29b0d0adc8b7
Daniel Augurell <Daniel.Augurell@alektumgroup.com>
2021-12-14 14:15:14 +0100

    slight refactor

--- 14/14.py
+++ 14/14.py
@@ -1,12 +1,14 @@
+from os import pardir
 from typing import Counter
 
-def step(pairs, instructions, letters, steps = 1):
+def step(pairs, instructions, letters, steps):
     for _ in range (steps):
         newPairs = Counter()
-        for pair in pairs:
-            newPairs[(pair[0],instructions[pair])] += pairs[pair]
-            newPairs[(instructions[pair],pair[1])] += pairs[pair]
-            letters[instructions[pair]] += pairs[pair]
+        for pair, sum in pairs.items():
+            insertletter = instructions[pair]
+            newPairs[(pair[0], insertletter)] += sum
+            newPairs[(insertletter, pair[1])] += sum
+            letters[insertletter] += sum
         pairs = newPairs
     return pairs, letters
 
@@ -14,7 +16,7 @@ with open("input.txt") as f:
     
     template, instructions = [line for line in f.read().split("\n\n")]
     instructions = [line.split(" -> ") for line in instructions.split("\n")]
-    instructions = {tuple(line[0]) : line[1] for line in instructions}
+    instructions = {tuple(pair) : letter for pair, letter in instructions}
 
     pairs = Counter(zip(template, template[1:]))
     letters = Counter(template)
@@ -24,4 +26,5 @@ with open("input.txt") as f:
     
     pairs, letters = step(pairs, instructions, letters, 30)
     print(max(letters.values()) - min(letters.values()))
+
 
