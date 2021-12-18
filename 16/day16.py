d27efb5736a094704f467711c3d2f4ecb56351be
Daniel Augurell <daniel.augurell@gmail.com>
2021-12-18 17:09:45 +0100

    small change

--- 16/day16.py
+++ 16/day16.py
@@ -4,8 +4,7 @@ from operator import gt, lt, eq
 from math import prod
 from time import time
 
-ops = [sum, prod, min, max]
-comps = [gt, lt, eq]
+ops = [sum, prod, min, max, None, gt, lt, eq]
 
 def sum_version(packets):
     if isinstance(packets, list):
@@ -17,28 +16,29 @@ def sum_version(packets):
 def calc_packet(_, T, value):
     if T == 4:
         return value
+    packets = [calc_packet(*p) for p in value]
     if T < 4:
-        return ops[T](calc_packet(*p) for p in value)
-    return int(comps[T-5](calc_packet(*value[0]), calc_packet(*value[1])))
+        return ops[T](packets)
+    return int(ops[T](*packets))
 
 def unpack_packet(packet: deque):
-    V = int("".join([packet.popleft() for _ in range(3)]), 2)
-    T = int("".join([packet.popleft() for _ in range(3)]), 2)
+    version = int("".join([packet.popleft() for _ in range(3)]), 2)
+    packetid = int("".join([packet.popleft() for _ in range(3)]), 2)
 
-    if T == 4:
+    if packetid == 4:
         value = ""
         while True:
             lastgroup = packet.popleft()
             value += "".join([packet.popleft() for _ in range(4)])
             if lastgroup != "1":
                 break
-        return (V, T, int(value,2))
+        return (version, packetid, int(value,2))
 
-    I = int(packet.popleft(), 2)
-    packetlen = int("".join([packet.popleft() for _ in range(11 if I else 15)]), 2)
+    lengthid = int(packet.popleft(), 2)
+    packetlen = int("".join([packet.popleft() for _ in range(11 if lengthid else 15)]), 2)
 
     subpackets = []
-    if I:
+    if lengthid:
         for _ in range(packetlen):
             subpackets.append(unpack_packet(packet))
 
@@ -46,7 +46,7 @@ def unpack_packet(packet: deque):
         length = len(packet)
         while length - len(packet) < packetlen:
             subpackets.append(unpack_packet(packet))
-    return (V, T, subpackets)
+    return (version, packetid, subpackets)
 
 
 def main():
@@ -56,7 +56,6 @@ def main():
         binary_packet = deque(bin(int(hexvalue, 16))[2:].zfill(len(hexvalue)*4))
 
         packet = unpack_packet(binary_packet)
-
 
         print(sum_version(packet))
         print(calc_packet(*packet))
